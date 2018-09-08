a0b9a54a63dc92bc960dff32c35a3a21a80916aa
DESKTOP-MB0RN9G\Nalbi_Min_Winodw <pyopie@nalbi.ai>
2018-09-08 19:07:26 +0900

    1234

--- request/views.py
+++ request/views.py
@@ -100,17 +100,20 @@ def ffmpeg_extract_subclip22(filename, t1, t2, targetname=None):
     """ Makes a new video file playing video file ``filename`` between
         the times ``t1`` and ``t2``. """
     name, ext = os.path.splitext(filename)
+    print("test40 = ")
     if not targetname:
         T1, T2 = [int(1000 * t) for t in [t1, t2]]
+        print("test41 = ")
         targetname = "%sSUB%d_%d.%s" % (name, T1, T2, ext)
-
+        print("test42 = ")
     cmd = [get_setting("FFMPEG_BINARY"), "-y",
            "-i", filename,
            "-ss", "%0.2f" % t1,
            "-t", "%0.2f" % (t2 - t1),
            "-vcodec", "copy", "-acodec", "copy", targetname]
-
+    print("test43 = ")
     subprocess_call(cmd)
+    print("test44 = ")
 
 def sound_ex(high):
     video_name = BASE_DIR + high.file_in.url
